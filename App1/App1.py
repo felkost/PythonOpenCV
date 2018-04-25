d611b953c49c890642df2e71c35050df2ca7e0f2
felkost <felkost@ukr.net>
2018-04-25 10:44:46 +0300

    Add project files.

---
+++ App1/App1.py
@@ -0,0 +1,5 @@
+#Reading, displaying images
+import cv2
+img = cv2.imread('img.jpg')
+cv2.imshow('Input image', img)
+cv2.waitKey()
---
+++ App1/App2.py
@@ -0,0 +1,6 @@
+#Loading and saving an gray image
+import cv2
+gray_img = cv2.imread('img.jpg', cv2.IMREAD_GRAYSCALE)
+cv2.imshow('Grayscale', gray_img)
+cv2.imwrite('output_img.jpg', gray_img)
+cv2.waitKey()
